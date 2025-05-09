4abbc875a3c66837da476d85b68899f0bd90930c
claudio schimizzi <bastaparavos@hotmail.com>
2025-05-09 09:17:05 -0300

    primer_commit

--- web1/urls.py
+++ web1/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from .views import lista_estudiantes, detalle_estudiante
 
+app_name = "web1"
+
 urlpatterns = [
     path('estudiantes/', lista_estudiantes, name='lista_estudiantes'),
     path('estudiantes/<int:pk>/', detalle_estudiante, name='detalle_estudiante'),
